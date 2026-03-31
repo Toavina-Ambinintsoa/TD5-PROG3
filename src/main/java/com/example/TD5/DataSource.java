b53d54bd1702d2eb4407e1a6e2142e229a87a489
Toavandr <hei.toavina.2@gmail.com>
2026-03-31 10:00:55 +0300

    fix:remove password

--- src/main/java/com/example/TD5/DataSource.java
+++ src/main/java/com/example/TD5/DataSource.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DataSource {
     private static final String URL = "jdbc:postgresql://localhost:5432/mini_dish_db";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "toavina";
+    private static final String PASSWORD = "*******";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
